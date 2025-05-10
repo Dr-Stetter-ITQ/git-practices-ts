f5a9e80bbdada667d64301e1c6a1c295b568e400
Reyes-ITQ <reyes@stetter-itq.com>
2025-05-10 20:53:53 +0100

    first step

---
+++ scripts/01-first-steps.ps1
@@ -0,0 +1,21 @@
+Write-Host "Setting back excersice 01-first-step"
+
+# Navigate to root repo
+Set-Location -Path (Split-Path -Parent $MyInvocation.MyCommand.Definition)
+cd ..
+
+# Reset files
+git checkout main
+git reset --hard HEAD
+git clean -fd
+
+# Create a the excersice folder
+New-Item -ItemType Directory -Path "01-first-steps" | Out-Null
+Set-Content -Path "01-first-steps\hello.ts" -Value 'console.log("Hola mundo");'
+
+# Commit and push changes
+git add 01-first-steps/hello.ts
+git commit -m "reset excersice"
+git push 
+
+Write-Host "Done!"
